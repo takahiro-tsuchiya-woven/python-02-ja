73bd568d1a497b7d27df2eed2acc6ca7a12d1991
takahiro-tsuchiya-woven <takahiro.tsuchiya@woven.toyota>
2024-05-16 19:10:56 +0900

    Updated apply_discount

--- 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
+++ 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
@@ -1,2 +1,20 @@
 def apply_discount(products, minimum_price, discount_rate):
-    pass
+    ##filtered_product = [(product["name"], product["price"]) for product in products if product["price"] > minimum_price]
+    filtered_product = [(lambda x: (x["name"], x["price"] * (100 - discount_rate) / 100))(product) for product in products if product["price"] > minimum_price]
+
+    return filtered_product
+
+input1 = [
+    {"name": "Laptop", "category": "Electronics", "price": 1200},
+    {"name": "Bread", "category": "Food", "price": 2},
+    {"name": "Jacket", "category": "Apparel", "price": 100}
+]
+
+input2 = [
+    {"name": "Smartphone", "category": "Electronics", "price": 800},
+    {"name": "Sneakers", "category": "Footwear", "price": 120},
+    {"name": "Coffee", "category": "Beverage", "price": 5}
+]
+
+print(apply_discount(input1, 50, 10))
+print(apply_discount(input2, 100, 15))
