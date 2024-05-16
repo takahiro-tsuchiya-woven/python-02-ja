f9104f3f6b59d99b5c89a109a19c5f74dead3d9d
takahiro-tsuchiya-woven <takahiro.tsuchiya@woven.toyota>
2024-05-16 15:20:01 +0900

    Updated functions

--- 01_intermediate_functions_and_data_structures/01_challenge/extract_data.py
+++ 01_intermediate_functions_and_data_structures/01_challenge/extract_data.py
@@ -1,7 +1,13 @@
 import re
 
 def extract_data(text):
-    pass
+    pattern = r'\b[0-9a-zA-Z]+:[0-9a-zA-Z]+\b'
+    matches = re.findall(pattern, text)
+
+    return matches
 
 def better_extract_data(text):
-    pass
+    pattern = r'\b([0-9a-zA-Z]+):([0-9a-zA-Z]+)\b'
+    matches = re.findall(pattern, text)
+
+    return matches
