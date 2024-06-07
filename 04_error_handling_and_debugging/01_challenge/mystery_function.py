6f57f467efc10047df998272a1eb495c70ff542b
takahiro-tsuchiya-woven <takahiro.tsuchiya@woven.toyota>
2024-06-07 17:28:43 +0900

    Fix code

--- 04_error_handling_and_debugging/01_challenge/mystery_function.py
+++ 04_error_handling_and_debugging/01_challenge/mystery_function.py
@@ -1,6 +1,12 @@
 def mystery_function(lst):
-    result = lst
+    result = lst.copy()
     for i in range(len(lst)):
-        if i % 2 == 0:
-            result[i] = i ** 2
+        if lst[i] % 2 == 0:
+            result[i] = lst[i] ** 2
     return result
+
+result_1 = mystery_function([1, 2, 3, 4, 5])
+print(result_1)
+
+result_2 = mystery_function([4, 1, 6, 2, 10])
+print(result_2)
